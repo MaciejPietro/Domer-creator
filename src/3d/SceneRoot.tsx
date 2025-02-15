8b3100147c18290d9acf860d3a49becee2c65414
Empedev <maciej@cleancommit.io>
2025-02-15 23:43:38 +0100

    add basic FPV controller

--- src/3d/SceneRoot.tsx
+++ src/3d/SceneRoot.tsx
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { useMemo, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { FlyControls, KeyboardControls, OrbitControls, PivotControls } from '@react-three/drei';
+import { FlyControls, KeyboardControls, OrbitControls, PivotControls, PointerLockControls } from '@react-three/drei';
 import { Geometry, Base, Subtraction, Addition } from '@react-three/csg';
 import { Environment } from '@/3d/Enviroment';
 import { DoubleSide } from 'three';
@@ -12,6 +12,8 @@ import { useStore } from '@/stores/EditorStore';
 import { ViewMode } from '@/2d/editor/constants';
 import House from './House';
 import { calculateCenterPoint } from './utils/helpers';
+import { Physics } from '@react-three/rapier';
+import FPVController, { World } from './controlers/FPVController';
 
 export default function SceneRoot() {
     const plan = FloorPlan.Instance.getPlanForModel();
@@ -23,13 +25,31 @@ export default function SceneRoot() {
     return (
         <>
             {plan.wallNodes.length ? (
-                <Canvas style={{ height: '100vh' }} shadows camera={{ position: [-60, 40, 15], fov: 25 }}>
+                <Canvas style={{ height: '100vh' }} camera={{ position: [0, 1, 5] }} flat shadows dpr={[1, 2]}>
+                    {/* // <Canvas style={{ height: '100vh' }} shadows camera={{ position: [-60, 40, 15], fov: 25 }}> */}
                     <axesHelper args={[10]} />
                     <gridHelper args={[200, 200, 0xff0000, 'teal']} />
-                    <House plan={plan} />
                     <Environment />
-                    <FlyControls autoForward={false} dragToLook={false} movementSpeed={1} rollSpeed={1} makeDefault />
                     {/* <OrbitControls makeDefault target={centerPoint} /> */}
+
+                    <Physics>
+                        {/* <Debug /> */}
+                        <House plan={plan} />
+                        <World />
+
+                        <KeyboardControls
+                            map={[
+                                { name: 'forwardKeyPressed', keys: ['ArrowUp', 'KeyW'] },
+                                { name: 'rightKeyPressed', keys: ['ArrowRight', 'KeyD'] },
+                                { name: 'backwardKeyPressed', keys: ['ArrowDown', 'KeyS'] },
+                                { name: 'leftKeyPressed', keys: ['ArrowLeft', 'KeyA'] },
+                            ]}
+                        >
+                            <FPVController />
+                        </KeyboardControls>
+                    </Physics>
+
+                    <PointerLockControls />
                 </Canvas>
             ) : (
                 <div className="w-full h-screen flex justify-center items-center gap-2 text-xl text-gray-800">
